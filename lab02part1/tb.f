73f564ee58e3ce1cd209d9ab8c8bbf03f063bb6c
klokaj <krzysiek.lokaj@gmail.com>
2020-11-05 14:07:22 +0100

    change files paths

--- lab02part1/tb.f
+++ lab02part1/tb.f
@@ -1,18 +1,22 @@
 mtm_alu_pkg.sv
 
-crc_calc.sv
-serial_monitor.sv
-output_decoder.sv
-input_decoder.sv
-random_data_gen.sv
-mtm_alu_model.sv
-
+my_src/crc_calc.sv
+my_src/serial_monitor.sv
+my_src/output_decoder.sv
+my_src/input_decoder.sv
+my_src/random_data_gen.sv
+my_src/mtm_alu_model.sv
 
 top.sv
 coverage.svh
 scoreboard.svh
 tester.svh
 mtm_alu_bfm.svh
+
+
+
+
+
 
 
 
