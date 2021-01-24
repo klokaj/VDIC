2aa6a6f09cb89dcfc3e881d667fe48b242d26db2
klokaj <krzysiek.lokaj@gmail.com>
2021-01-24 22:42:14 +0100

    Add tests to script

--- lab07/run_xrun_simple.sh
+++ lab07/run_xrun_simple.sh
@@ -2,6 +2,7 @@ xrun \
 	-F tb.f -F dut.f \
 	-uvm \
 	-uvmhome /cad/XCELIUM1909/tools/methodology/UVM/CDNS-1.2/sv \
-	+UVM_TESTNAME=kl_alu_example_test \
+	+UVM_TESTNAME=kl_alu_random_test \
+	+UVM_TESTNAME=kl_alu_minmax_test \
 	"$@" 
 
